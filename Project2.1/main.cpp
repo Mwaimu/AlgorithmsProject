6493d3709beeb492865336bcb1a73f65cdd96935
Nathan Welch <nawwx3@mst.edu>
2017-04-29 16:55:43 -0500

    Added may comments

--- Project2.1/main.cpp
+++ Project2.1/main.cpp
@@ -1,122 +1,151 @@
-// C++ program for implementation of Ford Fulkerson algorithm
+/*  Algorithms Project 2
+ * Shelby Luttrell, Ben Simpson, Nathan Welch
+ *
+ * Description:
+ *  Compare attacking algorithms (static and reactive) on a network that will (reactive) and won't (static)
+ *  recalculate maxFlow after each attack.
+ */
+
 #include <iostream>
 #include <limits.h>
 #include <string.h>
 #include <stack>
-#include <typeinfo>
 
 using namespace std;
 
+///things to know/remember  and/or NOTES
+/* -------------------------  NOTES  ---------------------------------------------
+ * -------------------------------------------------------------------------------
+ * I forgot what I was going to put in here but if you have questions text me
+ *
+ * I believe that what we really needed (problem) for each of the algorithms was that
+ *    we didn't a way to access the augmenting paths outside of FFA link we needed
+ *    this fixes that
+ *
+ * There's some stuff marked "TODO"
+ *    that should probably pop up in yellow? -> Shelby
+ *                               and pink? -> Ben  that you can look at
+ *
+ * Also, this was just an idea that I had. May or ma not work so change as necessary
+ *
+ * This file is the only one that is needed. help.cpp is the code this was based off of
+ *    but this file alone is the program as of now
+ ---------------------------------------------------------------------------------
+ ---------------------------------------------------------------------------------*/
+
+
+
+//TODO: if we use this its going to have to be much larger than 6...
 // Number of vertices in given graph
 #define V 6
 
-/* Returns true if there is a path from source 's' to sink 't' in
-residual graph. Also fills parent[] to store the path */
-bool dfs(int rGraph[V][V], int s, int t, int parent[], int arr[V][V])
-{
+/* returns true if path fom source to dest in residual graph.
+ * parent keeps track of the path
+ * arr[][] keeps track of all the paths that the DFS finds
+ */
+bool DFS(int resGraph[V][V], int source, int dest, int parent[], int arr[V][V]) {
   static int iter = 0;
-  // Create a visited array and mark all vertices as not visited
+  //All vertices as not visited for each "node"
   bool visited[V];
   memset(visited, 0, sizeof(visited));
 
-  // Create a queue, enqueue source vertex and mark source vertex
-  // as visited
+  // Create a stack, stack source "node", source parent as "null", source visited
   stack<int> q;
-  q.push(s);
-  visited[s] = true;
-  parent[s] = -1;
+  q.push(source);
+  visited[source] = true;
+  parent[source] = -1;
 
-  // Standard BFS Loop
-  while (!q.empty())
-  {
-    int u = q.top();
+  //while stack not empty
+  while (!q.empty()) {
+    int u = q.top();  //u = next "node" in stack
     q.pop();
 
-    for (int v=0; v<V; v++)
-    {
-      if (visited[v]==false && rGraph[u][v] > 0)
-      {
-        q.push(v);
+    //for each of the "nodes"
+    for (int v = 0; v < V; v++) {
+      //if it's not visited and there is flow left in the graph
+      if (visited[v] == 0 && resGraph[u][v] > 0) {
+        q.push(v); //push visited "node" to stack
         parent[v] = u;
         visited[v] = true;
       }
     }
   }
-  cout << "iter = " << iter << "  ";
+  //assign parent[] to arr[][] to keep track of paths
   for(int g = 0; g < V; g++) {
     cout << parent[g] << " ";
     arr[iter][g] = parent[g];
   }
-  iter++;
+  iter++; //increment so the next path is remembered
   cout << endl;
-  // If we reached sink in BFS starting from source, then return
-  // true, else false
-  return (visited[t] == true);
+  //if dest is reached, then return true
+  return (visited[dest] == 0);
 }
 
-// Returns the maximum flow from s to t in the given graph
-int fordFulkerson(int graph[V][V], int s, int t, int arr[V][V])
-{
+///---------------------------------------------------------------------------------------------------------------------
+///---------------------------------------------------------------------------------------------------------------------
+
+//returns maxFlow, also (by reference) creates and keeps track of resPaths
+int fordFulkerson(int graph[V][V], int source, int dest, int arr[V][V]) {
   int u, v;
+  int resGraph[V][V]; //residual graph, resGraph[i][j] is resCap of edge(i, j) -> 0 means no edge
+  int parent[V]; //stores path
+  int maxFlow = 0;
+  int resCap = 21; //residual capacity -> init higher that capacities to make any capacity possible resCap
 
-  // Create a residual graph and fill the residual graph with
-  // given capacities in the original graph as residual capacities
-  // in residual graph
-  int rGraph[V][V]; // Residual graph where rGraph[i][j] indicates
-  // residual capacity of edge from i to j (if there
-  // is an edge. If rGraph[i][j] is 0, then there is not)
-  for (u = 0; u < V; u++)
-    for (v = 0; v < V; v++)
-      rGraph[u][v] = graph[u][v];
-
-  int parent[V]; // This array is filled by BFS and to store path
-
-  int max_flow = 0; // There is no flow initially
-
-  // Augment the flow while there is path from source to sink
-  while (dfs(rGraph, s, t, parent, arr)) {
-    // Find minimum residual capacity of the edges along the
-    // path filled by BFS. Or we can say find the maximum flow
-    // through the path found.
-    int path_flow = 21;
-    for (v=t; v!=s; v=parent[v])
-    {
-      u = parent[v];
-      path_flow = min(path_flow, rGraph[u][v]);
+  //set resGraph == graph -> when the path becomes zero, no more flow available
+  for (u = 0; u < V; u++) {
+    for (v = 0; v < V; v++) {
+      resGraph[u][v] = graph[u][v];
     }
-
-    // update residual capacities of the edges and reverse edges
-    // along the path
-    for (v=t; v != s; v=parent[v])
-    {
-      u = parent[v];
-      rGraph[u][v] -= path_flow;
-      rGraph[v][u] += path_flow;
-    }
-
-    // Add path flow to overall flow
-    max_flow += path_flow;
   }
 
-  // Return the overall flow
-  return max_flow;
+  //while flow, find augmenting paths
+  while (DFS(resGraph, source, dest, parent, arr)) {
+    //find min resCap of edges along found path
+    for (v=dest; v!=source; v=parent[v]) {
+      u = parent[v];
+      resCap = min(resCap, resGraph[u][v]);
+    }
+
+    //update flow along path found
+    for (v=dest; v != source; v=parent[v]) {
+      u = parent[v];
+      resGraph[u][v] -= resCap;
+      resGraph[v][u] += resCap;
+    }
+
+    maxFlow += resCap;
+  }
+
+  return maxFlow;
 }
 
-// Driver program to test above functions
-int main()
-{
-  int arr[V][V] = {};
+///---------------------------------------------------------------------------------------------------------------------
+///---------------------------------------------------------------------------------------------------------------------
+
+int main() {
+  int arr[V][V] = {}; //keeps track of residual paths
   // Let us create a graph shown in the above example
-  int graph[V][V] = { {0, 16, 13, 0, 0, 0},
-                      {0, 0, 10, 12, 0, 0},
-                      {0, 4, 0, 0, 14, 0},
-                      {0, 0, 9, 0, 0, 20},
-                      {0, 0, 0, 7, 0, 4},
-                      {0, 0, 0, 0, 0, 0}
-  };
+
+
+  //TODO: this will obviously have to be changed
+
+  //keeps track of capacity at edge(i, j)
+  int graph[V][V] = { {0, 16, 13,  0,  0,  0},
+                      {0,  0, 10, 12,  0,  0},
+                      {0,  4,  0,  0, 14,  0},
+                      {0,  0,  9,  0,  0, 20},
+                      {0,  0,  0,  7,  0,  4},
+                      {0,  0,  0,  0,  0,  0}   };
+
+  /* TODO: could change to maybe...
+   * cin >> node1 >> node2;
+   * graph[node1][node2] = rand % 20 + 1; //random number between 1 and 20
+   */
 
   cout << "The maximum possible flow is " << fordFulkerson(graph, 0, 5, arr) << endl;
 
+  //this just prints the the paths
   for(int i = 0; i < V; i++) {
     cout << i << endl;
     for(int j = 0; j < V; j++) {
